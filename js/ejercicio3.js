53a5386f6fdc10a8fbbdd0156f2159a77cee80b1
Julian <julianser@hotmail.com.ar>
2023-03-30 02:45:55 -0300

    Se hizo la funcion agregarTarea

--- js/ejercicio3.js
+++ js/ejercicio3.js
@@ -1,2 +1,32 @@
 //3 - Crea una web con bootstrap y js, que contenga un botón input donde se pueda cargar una tarea y un botón que al ser presionado agregue dicha tarea a una lista, cada elemento ingresado en la lista debe poder ser eliminado con un botón creado para ese fin. 
 
+
+const btnAgregar = document.getElementById('agregar');
+btnAgregar.addEventListener('click',agregarTarea) 
+
+
+function agregarTarea(e)
+{   
+    const padre = document.querySelector('form')
+    e.preventDefault()
+    let input = document.querySelector('input')
+    let dato = input.value;
+    let lugar = document.createElement('li')
+    lugar.innerHTML = `${dato}`
+    padre.appendChild(lugar);
+}
+
+function borrarTarea(){
+    
+}
+
+
+
+
+
+    // document.write("<ul>")
+    // for(let i=0; i<listatareas.length;i++)
+    // {
+    //     document.write(`<li>${listatareas[i]}</li>`)
+    // }
+    // document.write("</ul>")
